d4eea27d0089cf9158d8f973d5595e89f87d90a9
kpk-abs <kpk@abstractit.in>
2023-11-04 13:12:52 +0530

    Introduced normalize config module.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { React } from 'react';
 import './App.scss';
-import Tile from './components/Tile';
+import Tile from './component/Tile';
+import normalizeConfig from './services/normalizeConfig';
 
-const App = (context) =>
-	<div className="App">
-		<Tile { ...{ ...context, data: { name: 'brinjal', value: 1 }} }/>
+const App = (context) => {
+	const normalizedConfig = normalizeConfig(context);
+
+	return <div className="App">
+		<Tile {
+			...{
+				...context,
+				data: { name: 'capsicum', value: 2 },
+				config: normalizedConfig,
+			}
+		}
+		/>
 	</div>;
+};
 
 export default App;
