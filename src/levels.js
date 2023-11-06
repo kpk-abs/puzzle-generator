1e8978b4bcb3d55d373436d083f41316e20f5f1b
dmdm-abs <dmdm@abstractit.in>
2023-11-06 15:48:55 +0530

    Refactored levels.

--- src/levels.js
+++ src/levels.js
@@ -1,44 +1,38 @@
-const levels = [
-	{
-		name: 'level1',
-		items: {
-			tiles: [
-				{
-					display: 'image',
-					value: 1,
-				},
-				{
-					display: 'image',
-					value: 1,
-				},
-			],
-			boxes: [
-				{
-					maxTiles: 2,
-				},
-			],
-		},
+const levels = {
+	level1: {
+		tiles: [
+			{
+				display: 'image',
+				value: 1,
+			},
+			{
+				display: 'image',
+				value: 1,
+			},
+		],
+		boxes: [
+			{
+				maxTiles: 2,
+			},
+		],
 	},
-	{
-		name: 'level2',
-		items: {
-			tiles: [
-				{
-					display: 'image',
-					value: 1,
-				},
-				{
-					display: 'image',
-					value: 2,
-				},
-			],
-			boxes: [
-				{
-					maxTiles: 3,
-				},
-			],
-		},
+	level2: {
+		tiles: [
+			{
+				display: 'image',
+				value: 1,
+			},
+			{
+				display: 'image',
+				value: 2,
+			},
+		],
+		boxes: [
+			{
+				maxTiles: 3,
+			},
+		],
 	},
-];
+};
 
 export default levels;
