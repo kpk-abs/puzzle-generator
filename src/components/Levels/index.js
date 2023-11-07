81edfe3b2fc3c7607eb274d206e5e4c67704f5ae
njj-abs <njj@abstractit.in>
2023-11-07 15:50:19 +0530

    Changed the Level data by generateLevel.

--- src/components/Levels/index.js
+++ src/components/Levels/index.js
@@ -1,30 +1,18 @@
 import { map, values } from '@laufire/utils/collection';
 import React from 'react';
 import Level from '../Level';
-
-const data = [
-	{
-		name: 'pencil',
-		display: 'image',
-		group: 'a',
-		type: 'tile',
-		value: 1,
-	},
-	{
-		type: 'box',
-		value: 2,
-	},
-];
+import generateLevel from '../../generateLevel';
 
 const Levels = ({ levels, ...context }) => {
 	const { state: { currentLevel }} = context;
+	const { data = [] } = currentLevel && generateLevel(currentLevel);
 
 	const levelElements = values(map(levels, ({ name }, key) =>
 		<div
 			key={ key }
 			onClick={ () => { context.actions.setCurrentLevel(key); } }
 		>
-			{name}
+			{ name }
 		</div>));
 
 	const levelProps = { ...context, data };
