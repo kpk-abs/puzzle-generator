b64acba89b617019cf6b9fb00a62918acff43403
dmdm-abs <dmdm@abstractit.in>
2023-11-07 10:26:10 +0530

    Removed reduceSync in helpers.

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,32 +1,11 @@
-import { keys, merge } from '@laufire/utils/collection';
+import { merge, reduce } from '@laufire/utils/collection';
 
-const reduceSync = async (
-	collection, reducer, initial,
-) => {
-	let acc = initial;
-
-	const indexes = keys(collection);
-	const indexLength = indexes.length;
-
-	for(let i = 0; i < indexLength; i++) {
-		const index = indexes[i];
-
-		// eslint-disable-next-line no-await-in-loop
-		acc = await reducer(
-			acc, collection[index], index, collection,
-		);
-	}
-
-	return acc;
-};
-
-const pipe = (pipes, data) => reduceSync(
+const pipe = (pipes, data) => reduce(
 	pipes, async (acc, c) => merge(
 		{}, acc, await c(acc) || {}
 	), data,
 );
 
 export {
-	reduceSync,
 	pipe,
 };
