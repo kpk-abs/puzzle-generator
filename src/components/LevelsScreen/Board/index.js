f70a54b359d39b64ccf98ebe389ccf7154ad7ce8
njj-abs <njj@abstractit.in>
2023-11-08 19:02:31 +0530

    Board for background.

--- src/components/LevelsScreen/Board/index.js
+++ src/components/LevelsScreen/Board/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import GameObject from '../../GameObject';
-
+import background from '../../../Images/background.png';
 const Board = ({ data, ...context }) =>
-	<div className="board">{
-		data.map((prop, key) =>
-			<GameObject key={ key } { ...{ ...context, data: prop } }/>)
-	}</div>;
+	<div
+		className="board"
+		style={ { background: `url(${ background })` } }
+	>{
+			data.map((prop, key) =>
+				<GameObject key={ key } { ...{ ...context, data: prop } }/>)
+		}
+	</div>;
 
 export default Board;
