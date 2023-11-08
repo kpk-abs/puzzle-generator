1419751603971f380617c85f296bb135367217c1
dmdm-abs <dmdm@abstractit.in>
2023-11-08 16:30:22 +0530

    Introduced genTile.

---
+++ src/services/buildSimpleStack/genTile.js
@@ -0,0 +1,13 @@
+import { rndBetween } from '@laufire/utils/lib';
+import { generateID } from '../../helpers';
+
+const genTile = ({ variation, group, value: { min, max }}) => {
+	const name = 'pencil';
+	const type = 'tile';
+	const value = rndBetween(min, max);
+	const id = generateID();
+
+	return { id, name, variation, group, type, value };
+};
+
+export default genTile;
--- src/services/buildSimpleStack/genTiles.js
+++ src/services/buildSimpleStack/genTiles.js
@@ -1,21 +1,13 @@
 import { map, range } from '@laufire/utils/collection';
-import { rndBetween } from '@laufire/utils/lib';
 import { rndValue } from '@laufire/utils/random';
-import { generateID } from '../../helpers';
+import genTile from './genTile';
 
 const genTiles = (tile) => {
-	const { count, variations, value: { min, max }, groups } = tile;
-	const name = 'pencil';
-	const type = 'tile';
+	const { count, variations, groups } = tile;
 	const variation = rndValue(variations);
 	const group = rndValue(groups);
 
-	return map(range(0, count), () => {
-		const value = rndBetween(min, max);
-		const id = generateID();
-
-		return { id, name, variation, group, type, value };
-	});
+	return map(range(0, count), () => genTile({ ...tile, variation, group }));
 };
 
 export default genTiles;
