5395ba71f9b1c2fdb5f830a6c316ced12c6b970d
dmdm-abs <dmdm@abstractit.in>
2023-11-08 11:44:46 +0530

    Refactored generateBoxes.

--- src/services/buildSimpleStack/generateBoxes.js
+++ src/services/buildSimpleStack/generateBoxes.js
@@ -1,8 +1,8 @@
 import { map, range } from '@laufire/utils/collection';
 import { rndBetween } from '@laufire/utils/lib';
 
-const generateBoxes = ({ data, box: { count, value: { min, max }}}) => {
-	const generatedBoxes = map(range(0, count), () => ({
+const generateBoxes = ({ data, boxes, box: { count, value: { min, max }}}) => {
+	const generatedBoxes = boxes || map(range(0, count), () => ({
 		type: 'box',
 		value: rndBetween(min, max),
 	}));
