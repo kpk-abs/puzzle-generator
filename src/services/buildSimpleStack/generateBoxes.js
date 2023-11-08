3df6e9e4e07ac5f36aa9e5e6d65ee8f06754d9b2
dmdm-abs <dmdm@abstractit.in>
2023-11-08 11:52:54 +0530

    Added id in generateBoxes.

--- src/services/buildSimpleStack/generateBoxes.js
+++ src/services/buildSimpleStack/generateBoxes.js
@@ -1,8 +1,10 @@
 import { map, range } from '@laufire/utils/collection';
 import { rndBetween } from '@laufire/utils/lib';
+import { generateID } from '../../helpers';
 
 const generateBoxes = ({ data, boxes, box: { count, value: { min, max }}}) => {
 	const generatedBoxes = boxes || map(range(0, count), () => ({
+		id: generateID(),
 		type: 'box',
 		value: rndBetween(min, max),
 	}));
