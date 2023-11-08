c090fa2a501cdfaa72fd4153d5429906e3179eac
dmdm-abs <dmdm@abstractit.in>
2023-11-08 16:03:40 +0530

    Introduced genBox.

---
+++ src/services/buildSimpleStack/genBox.js
@@ -0,0 +1,19 @@
+import { rndBetween } from '@laufire/utils/lib';
+import { generateID } from '../../helpers';
+
+const genBox = ({ items = [], value: { min, max }}) => {
+	const maxPosition = 101;
+
+	return {
+		id: generateID(),
+		type: 'box',
+		position: {
+			x: rndBetween(0, maxPosition),
+			y: rndBetween(0, maxPosition),
+		},
+		value: rndBetween(min, max),
+		items: items,
+	};
+};
+
+export default genBox;
--- src/services/buildSimpleStack/genBoxes.js
+++ src/services/buildSimpleStack/genBoxes.js
@@ -1,21 +1,6 @@
 import { map, range } from '@laufire/utils/collection';
-import { rndBetween } from '@laufire/utils/lib';
-import { generateID } from '../../helpers';
+import genBox from './genBox';
 
-const genBoxes = ({ count, items = [], value: { min, max }}) =>
-	map(range(0, count), () => {
-		const maxPosition = 101;
-
-		return {
-			id: generateID(),
-			type: 'box',
-			position: {
-				x: rndBetween(0, maxPosition),
-				y: rndBetween(0, maxPosition),
-			},
-			value: rndBetween(min, max),
-			items: items,
-		};
-	});
+const genBoxes = (box) => map(range(0, box.count), () => genBox(box));
 
 export default genBoxes;
