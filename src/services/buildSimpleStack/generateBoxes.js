25059af56197ef7ebf005b3a113ae3a5a9ff3cbd
dmdm-abs <dmdm@abstractit.in>
2023-11-08 13:11:51 +0530

    Introduced items in generateBoxes.

--- src/services/buildSimpleStack/generateBoxes.js
+++ src/services/buildSimpleStack/generateBoxes.js
@@ -2,11 +2,16 @@ import { map, range } from '@laufire/utils/collection';
 import { rndBetween } from '@laufire/utils/lib';
 import { generateID } from '../../helpers';
 
-const generateBoxes = ({ data, boxes, box: { count, value: { min, max }}}) => {
+const generateBoxes = ({
+	data,
+	boxes,
+	box: { count, items = [], value: { min, max }},
+}) => {
 	const generatedBoxes = boxes || map(range(0, count), () => ({
 		id: generateID(),
 		type: 'box',
 		value: rndBetween(min, max),
+		items: items,
 	}));
 
 	return {
