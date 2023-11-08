a4f40f91fc4f9f379146034aa55c16fe8efada11
dmdm-abs <dmdm@abstractit.in>
2023-11-08 17:03:29 +0530

    Introduced position in genTile.

--- src/services/buildSimpleStack/genTile.js
+++ src/services/buildSimpleStack/genTile.js
@@ -4,10 +4,15 @@ import { generateID } from '../../helpers';
 const genTile = ({ variation, group, value: { min, max }}) => {
 	const name = 'carrot';
 	const type = 'tile';
+	const maxPosition = 101;
 	const value = rndBetween(min, max);
 	const id = generateID();
+	const position = {
+		x: rndBetween(0, maxPosition),
+		y: rndBetween(0, maxPosition),
+	};
 
-	return { id, name, variation, group, type, value };
+	return { id, name, variation, group, type, value, position };
 };
 
 export default genTile;
