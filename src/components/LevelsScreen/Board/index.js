06c74da882c81b6566d7d5a6252c4dfa35bf3ff3
njj-abs <njj@abstractit.in>
2023-11-09 17:55:12 +0530

    Created the BoardContainer.

--- src/components/LevelsScreen/Board/index.js
+++ src/components/LevelsScreen/Board/index.js
@@ -2,24 +2,39 @@ import React from 'react';
 import GameObject from '../../GameObject';
 import background from '../../../Images/background.png';
 
-const Board = ({ data, ...context }) =>
-	<div
-		className="board"
-		style={ { background: `url(${ background })` } }
-	>{
-			data.map((prop, key) => {
-				const { x, y } = prop.position;
+const BoardItems = (context) =>
+	context.data.map((prop, key) => {
+		const { x, y } = prop.position;
 
-				return (
-					<div
-						key={ key }
-						className="boardItem"
-						style={ { left: `${ x }%`, top: `${ y }%` } }
-					>
-						<GameObject { ...{ ...context, data: prop } }/>
-					</div>);
-			})
-		}
-	</div>;
+		return (
+			<div
+				key={ key }
+				className="boardItem"
+				style={ { left: `${ x }%`, top: `${ y }%` } }
+			>
+				<GameObject { ...{ ...context, data: prop } }/>
+			</div>);
+	});
+
+const Board = (context) => {
+	const { config: { ratioMultiplier: { width, height }}} = context;
+
+	return (
+		<div
+			className="board"
+			style={ { background: `url(${ background })` } }
+		>
+			<div
+				className="boardContainer"
+				style={ {
+					width: `calc(100dvw * ${ width })`,
+					height: `calc(100dvh * ${ height })`,
+				} }
+			>
+				<BoardItems { ...context }/>
+			</div>
+		</div>
+	);
+};
 
 export default Board;
