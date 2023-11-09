a5038e6a964da05f06ed32bd6958d8094b484c37
nal-abs <nal@abstractit.in>
2023-11-09 19:07:26 +0530

    Refactored arguments passed to box component.

--- src/stories/Box.stories.js
+++ src/stories/Box.stories.js
@@ -10,31 +10,31 @@ const component = {
 
 export default component;
 
-const Template = (args) => {
-	const normalizedConfig = normalizeConfig.normalizeSprite(context);
+const normalizedConfig = normalizeConfig.normalizeSprite(context);
 
-	return (
-		<BoxComponent { ...{
-			...context, config: normalizedConfig,
-			state: context.seed, ...args,
-		} }
-		/>);
-};
+const Template = (args) =>
+	<BoxComponent { ...{
+		...context, config: normalizedConfig,
+		state: context.seed, ...args,
+	} }
+	/>;
 
 export const Box = Template.bind({});
 
 Box.args = {
 	data: {
-		items: {
-			image: 'carrot',
-			variation: 'image',
-			group: 'a',
-			type: 'tile',
-			value: 1,
-			position: {
-				x: 36,
-				y: 79,
+		items: [
+			{
+				image: 'carrot',
+				variation: 'image',
+				group: 'a',
+				type: 'tile',
+				value: 1,
+				position: {
+					x: 36,
+					y: 79,
+				},
 			},
-		},
+		],
 	},
 };
