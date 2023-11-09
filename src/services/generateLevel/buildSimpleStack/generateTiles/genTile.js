e55fab785fc17a271980d7fea8db5ce4adddb6c2
dmdm-abs <dmdm@abstractit.in>
2023-11-09 13:57:26 +0530

    Refactored.

--- src/services/generateLevel/buildSimpleStack/generateTiles/genTile.js
+++ src/services/generateLevel/buildSimpleStack/generateTiles/genTile.js
@@ -2,9 +2,9 @@ import { rndBetween } from '@laufire/utils/lib';
 import { generateID } from '../../../../helpers';
 
 const genTile = ({ variation, group, value: { min, max }}) => {
-	const name = 'carrot';
 	const type = 'tile';
 	const maxPosition = 101;
+	const image = variation === 'image' ? { image: 'carrot' } : {};
 	const value = rndBetween(min, max);
 	const id = generateID();
 	const position = {
@@ -12,7 +12,7 @@ const genTile = ({ variation, group, value: { min, max }}) => {
 		y: rndBetween(0, maxPosition),
 	};
 
-	return { id, name, variation, group, type, value, position };
+	return { ...image, id, variation, group, type, value, position };
 };
 
 export default genTile;
