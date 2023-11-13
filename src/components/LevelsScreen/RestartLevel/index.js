3a0603636beaa1c7a9e659570e6bc46535322323
njj-abs <njj@abstractit.in>
2023-11-13 15:07:43 +0530

    Bug Fixed.

--- src/components/LevelsScreen/RestartLevel/index.js
+++ src/components/LevelsScreen/RestartLevel/index.js
@@ -1,7 +1,7 @@
 import { Button } from '@mui/material';
 import React from 'react';
 
-const RestartLevel = ({ actions, state: { name }}) =>
+const RestartLevel = ({ actions, state: { level: { name }}}) =>
 	<Button onClick={ () => actions.setLevel(name) }>
 		Restart
 	</Button>;
