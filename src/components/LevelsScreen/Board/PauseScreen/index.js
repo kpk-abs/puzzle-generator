ffd18eb2387c7ddac20ebbf5d8afd0aa8ca33239
njj-abs <njj@abstractit.in>
2023-11-14 15:51:03 +0530

    Changed the backDrop in the PauseScreen.

--- src/components/LevelsScreen/Board/PauseScreen/index.js
+++ src/components/LevelsScreen/Board/PauseScreen/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import Levels from './Levels';
 import RestartLevel from '../../RestartLevel';
-import { Backdrop } from '@mui/material';
 import Home from './Home';
 import Play from './Play';
 
 const components = [Levels, RestartLevel, Home, Play];
 
+const PauseContainer = (context) =>
+	<div className="pauseContainer">
+		{
+			components.map((Component, i) =>
+				<Component key={ i } { ...context }/>)
+		}
+	</div>;
+
 const PauseScreen = (context) => {
-	const { actions, state: { pause }} = context;
+	const { state: { pause }} = context;
 
 	return (
-		<Backdrop
-			open={ pause }
-			sx={ { zIndex: (theme) => theme.zIndex.drawer + 1 } }
-			onClick={ () => actions.setPause(!pause) }
-		>
-			{components.map((Component, i) =>
-				<Component key={ i }{ ...context }/>)}
-		</Backdrop>
+		pause
+		&& <div className="pause">
+			<PauseContainer { ...context }/>
+		</div>
 	);
 };
 
